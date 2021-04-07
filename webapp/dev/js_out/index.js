837c622c69814e0307eb21f352a86bd292147693
Sanford Pun <punsanford@gmail.com>
2021-04-07 17:57:41 +0800

    gitignore generated js files

--- webapp/dev/js_out/index.js
+++
@@ -1,8 +0,0 @@
-import { DrawingUtil } from "polypartition";
-var mainCanvas = document.getElementById('main');
-var mainCtx = mainCanvas.getContext('2d');
-mainCtx.fillStyle = "#FFFFFF";
-mainCtx.fillRect(0, 0, mainCanvas.width, mainCanvas.height);
-var drawingUtil = DrawingUtil.from_canvas_id('main');
-drawingUtil.draw_line(5, 4, 50, 100);
-//# sourceMappingURL=index.js.map
