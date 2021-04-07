0c1201548f08402bf1bea07483a0d1f2d18e04f1
Sanford Pun <punsanford@gmail.com>
2021-04-07 17:58:22 +0800

    TS + WASM test

--- webapp/dev/src/index.ts
+++ webapp/dev/src/index.ts
@@ -3,8 +3,14 @@ import { DrawingUtil } from "polypartition";
 const mainCanvas = document.getElementById('main') as HTMLCanvasElement;
 const mainCtx = mainCanvas.getContext('2d');
 
-mainCtx.fillStyle = "#FFFFFF";
-mainCtx.fillRect(0, 0, mainCanvas.width, mainCanvas.height);
+function clearCanvas(canvas: HTMLCanvasElement) {
+    let ctx = canvas.getContext('2d');
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+clearCanvas(mainCanvas);
 
 const drawingUtil = DrawingUtil.from_canvas_id('main');
 drawingUtil.draw_line(5, 4, 50, 100);
+drawingUtil.draw_line(400, 300, 250, 350);
