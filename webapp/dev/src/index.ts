a0fdb16dddcc4d527976dbb3a99d07df60047fe7
Sanford Pun <punsanford@gmail.com>
2021-04-16 16:23:13 +0800

    Show all test outputs

--- webapp/dev/src/index.ts
+++ webapp/dev/src/index.ts
@@ -8,17 +8,27 @@ async function main() {
         const testName = fn.name;
         let success = true;
 
+        // Create the HTML row in the table
+        const tr = document.createElement('tr');
+        const nameTd = document.createElement('td');
+        const canvasTd = document.createElement('td');
+        nameTd.innerHTML = testName;
+        canvasTd.innerHTML = `<canvas id="${testName}" width="352" height="416"></canvas>`;
+        tr.appendChild(nameTd);
+        tr.appendChild(canvasTd);
+        (document.getElementById('results_table') as HTMLTableElement).tBodies[0].appendChild(tr);
+
         console.groupCollapsed(testName);
         try {
-            await fn('main');
+            await fn(testName);
             console.log("%c" + testName + " Success!", "color: lime;");
         } catch (e) {
             console.error(e);
             success = false;
+        } finally {
+            console.groupEnd();
+            results[testName] = success? "Success" : "Fail";
         }
-        console.groupEnd();
-
-        results[testName] = success? "Success" : "Fail";
     }
 
     console.table(results);
